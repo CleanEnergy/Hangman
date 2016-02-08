d1b3833cac455420f9b0af67e66c1fb2ec72cbba
David Fenko <fenkodavid@live.com>
2016-02-08 13:32:21 +0100

    Create homepage & menu
    Simple menu layout.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hangman</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css"></link>
+    </head>
+    <body>
+        <?php include "menu.php"?>
+
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/jquery-2.2.0.min.js"></script>
+
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/index.php">Hangman</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="defaultNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Play</a></li>
+                <li><a href="#">History</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
